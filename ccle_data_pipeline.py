8284631ee1ffc1cf620f38832847b2da63568f73
John Vivian <jtvivian@gmail.com>
2016-12-13 00:31:49 -0800

    Finish process and upload step
    PEP

--- ccle_data_pipeline.py
+++ ccle_data_pipeline.py
@@ -1,13 +1,12 @@
 import argparse
 import os
-import shutil
-
-from toil_lib.programs import docker_call
-from toil.job import Job
-from bd2k.util.files import mkdir_p
-from toil_lib.urls import s3am_upload
-from toil_lib.jobs import map_job
+import tarfile
 from glob import glob
+
+from toil.job import Job
+from toil_lib.jobs import map_job
+from toil_lib.programs import docker_call
+from toil_lib.urls import s3am_upload
 
 
 def download_bam(job, gdc_id):
@@ -20,18 +19,22 @@ def download_bam(job, gdc_id):
     sample = glob(os.path.join(output_dir, '*.bam'))[0]
     bam_id = job.fileStore.writeGlobalFile(sample)
 
-    job.addChildJobFn(process_bam, bam_id, gdc_id)
+    job.addChildJobFn(process_bam_and_upload, bam_id, gdc_id, disk='100G')
 
 
-def process_bam(job, bam_id, gdc_id):
+def process_bam_and_upload(job, bam_id, gdc_id):
     work_dir = job.fileStore.getLocalTempDir()
     job.fileStore.readGlobalFile(bam_id, os.path.join(work_dir, 'input.bam'))
 
     parameters = ['fastq', '-1', '/data/r1.fastq', '-2', '/data/r2.fastq', '/data/input.bam']
     docker_call(tool='quay.io/ucsc_cgl/samtools', work_dir=work_dir, parameters=parameters)
 
+    out_tar = os.path.join(work_dir, gdc_id + '.tar.gz')
+    with tarfile.open(out_tar, 'w:gz') as tar:
+        for name in [os.path.join(work_dir, x) for x in ['r1.fastq', 'r2.fastq']]:
+            tar.add(name, arcname=os.path.basename(name))
 
-
+    s3am_upload(out_tar, s3_dir='s3://cgl-ccle-data/')
 
 
 def parse_gdc_manifest(manifest_path):
