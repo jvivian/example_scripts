2ea61ead4221b31d3a754e161ff64cf683f89808
John Vivian <jtvivian@gmail.com>
2016-12-20 12:33:55 -0800

    Add encryption to upload

--- transfer_beatAML_to_s3.py
+++ transfer_beatAML_to_s3.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 home_dir = '/pod/home/jvivian/beatAML-transfer/'
 sample_dir = '/pod/pstore/projects/BeatAML/fastq-12_07_2016/'
 s3_dir = 's3://cgl-beataml-data/'
+s3_key = '/pod/home/jvivian/master.key'
 
 samples = os.listdir(sample_dir)
 sample_ids = {x.split('_L00')[0] for x in samples}
@@ -22,6 +23,6 @@ for sample_id in tqdm(sample_ids):
         for sample in subset:
             tar.add(sample, arcname=os.path.basename(sample))
 
-    s3am_upload(out_tar, os.path.join(s3_dir, sample_id))
+    s3am_upload(out_tar, s3_dir, num_cores=6, s3_key_path=s3_key)
 
     os.remove(out_tar)
